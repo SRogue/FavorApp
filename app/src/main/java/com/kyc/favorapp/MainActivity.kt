88c111fa606a46d4b39760b987329cb9f82ffad3
SRouge <kyc4094376098@163.com>
2019-04-14 16:29:57 +0800

    init

--- app/src/main/java/com/kyc/favorapp/MainActivity.kt
+++ app/src/main/java/com/kyc/favorapp/MainActivity.kt
@@ -1,18 +1,15 @@
 package com.kyc.favorapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 
-    override fun onResume() {
-        super.onResume()
-        Toast.makeText(this,"66666",Toast.LENGTH_SHORT).show()
-    }
+
 }
